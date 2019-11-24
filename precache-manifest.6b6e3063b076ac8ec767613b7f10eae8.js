f382b5f56ccefe697dc0079a62991fb3d2550a2b
Jonathan C <jsea@uw.edu>
2019-11-23 16:35:47 -0800

    Updates

---
+++ precache-manifest.6b6e3063b076ac8ec767613b7f10eae8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82036734bb26c10021ec0d1adbc67ccb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4b480cedf622812a9f7",
+    "url": "/static/css/main.d5c8bd09.chunk.css"
+  },
+  {
+    "revision": "4f8ff3fea2057b51f763",
+    "url": "/static/js/2.723cc11f.chunk.js"
+  },
+  {
+    "revision": "b4b480cedf622812a9f7",
+    "url": "/static/js/main.d831b542.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  }
+]);
